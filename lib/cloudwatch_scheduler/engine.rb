c6bebec1c3da678dca1bd7a3b7d9c42063dea4a4
Nat Budin <natbudin@gmail.com>
2022-03-13 19:45:55 -0700

    Use a to_prepare block so that Zeitwerk doesn't break at load time

--- lib/cloudwatch_scheduler/engine.rb
+++ lib/cloudwatch_scheduler/engine.rb
@@ -3,20 +3,22 @@
 module CloudwatchScheduler
   class Engine < Rails::Engine
     initializer "cloudwatch_scheduler.setup_job" do
-      # Have to do this in initializer rather than require time because it
-      # inherits from ApplicationJob
-      require "cloudwatch_scheduler/job"
+      config.to_prepare do
+        # Have to do this in initializer rather than require time because it
+        # inherits from ApplicationJob
+        require "cloudwatch_scheduler/job"
 
-      # Explicitly register this worker, because Shoryuken expects the message
-      # attributes to specify the job class, and these jobs are produced by
-      # Cloudwatch Events, which provide no way to set message attributes
-      Shoryuken.worker_registry.register_worker(
-        CloudwatchScheduler::Job.queue_name,
-        ActiveJob::QueueAdapters::ShoryukenAdapter::JobWrapper
-      )
+        # Explicitly register this worker, because Shoryuken expects the message
+        # attributes to specify the job class, and these jobs are produced by
+        # Cloudwatch Events, which provide no way to set message attributes
+        Shoryuken.worker_registry.register_worker(
+          CloudwatchScheduler::Job.queue_name,
+          ActiveJob::QueueAdapters::ShoryukenAdapter::JobWrapper
+        )
 
-      # Load the configuration
-      require Rails.root.join("config/cloudwatch_schedule").to_s
+        # Load the configuration
+        require Rails.root.join("config/cloudwatch_schedule").to_s
+      end
     end
 
     rake_tasks do
